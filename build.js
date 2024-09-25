02a19f3159a8e9dd34233c463e33b9e7ee0ee27e
Mohammed Husamuddin <uni.husam@gmail.com>
2024-09-25 13:10:49 +0400

    updated resume

--- build.js
+++ build.js
@@ -1,7 +1,8 @@
 const fs = require('fs-extra');
 const path = require('path');
-const handlebars = require('handlebars');
+const Handlebars = require('handlebars');
 const moment = require('moment');
+const _ = require('underscore');
 
 const DIST_DIR = './dist';
 const RESUME_FILE = './resume.json';
@@ -9,31 +10,36 @@ const RESUME_FILE = './resume.json';
 const resumeData = require(RESUME_FILE);
 
 // Helper: breaklines
-handlebars.registerHelper('breaklines', function(text) {
-    text = handlebars.Utils.escapeExpression(text);
+Handlebars.registerHelper('breaklines', function(text) {
+    text = Handlebars.Utils.escapeExpression(text);
     text = text.replace(/(\r\n|\n|\r)/gm, '<br>');
-    return new handlebars.SafeString(text);
+    return new Handlebars.SafeString(text);
 });
 
 // Helper: getBuildDate
-handlebars.registerHelper('getBuildDate', function() {
+Handlebars.registerHelper('getBuildDate', function() {
     return moment().format('MMMM Do YYYY, h:mm:ss a');
 });
 
 // Helper: toLowerCase
-handlebars.registerHelper('toLowerCase', function(str) {
+Handlebars.registerHelper('toLowerCase', function(str) {
     return str.toLowerCase();
 });
 
 // Helper: join
-handlebars.registerHelper('join', function(arr, separator) {
+Handlebars.registerHelper('join', function(arr, separator) {
     return arr.join(separator);
+});
+
+// Helper: formatDate
+Handlebars.registerHelper('formatDate', function(dateString) {
+    return moment(dateString).format('MMMM YYYY');
 });
 
 async function main() {
   try {
     const template = await fs.readFile(path.join(__dirname, 'resume.hbs'), 'utf-8');
-    const compiledTemplate = handlebars.compile(template);
+    const compiledTemplate = Handlebars.compile(template);
     const html = compiledTemplate(resumeData);
 
     await fs.ensureDir(path.join(__dirname, DIST_DIR));
